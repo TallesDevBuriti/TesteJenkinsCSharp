29b1c4e3ae9182475d123f44adb0c91e222dfc7e
TallesDevBuriti <talles.machado@buritiempreendimentos.com.br>
2024-06-19 08:50:34 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteDeployJenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
